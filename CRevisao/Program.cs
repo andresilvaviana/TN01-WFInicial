1520e578c63eb33aff12841ad4786547bae5ed08
ANDRE SILVA VIANA <andre.silva.viana.1995@gmail.com>
2025-02-26 22:01:18 -0300

    Adicionar arquivos de projeto.

---
+++ CRevisao/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CRevisao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("informe a primera nota:");
+            double nota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("informe a segunda nota:");
+            double nota2 = Convert.ToDouble(Console.ReadLine());
+            
+            double res = (nota1 + nota2) / 2;
+
+            if (res >= 7)
+            {
+                Console.WriteLine("Aprovado!");
+            }
+            else if (res <=5)
+            {
+                Console.WriteLine("Reprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Recuperação!");
+            }
+        }
+    }
+}
---
+++ WFInicial/Form1.cs
@@ -0,0 +1,33 @@
+namespace WFInicial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
