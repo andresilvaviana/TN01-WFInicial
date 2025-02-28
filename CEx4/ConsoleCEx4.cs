cd6bc8c40772be89519e4db5ca9bee96ec6d0bda
ANDRE SILVA VIANA <andre.silva.viana.1995@gmail.com>
2025-02-28 19:19:30 -0300

    feat: Exercio 4 corrigido no console

---
+++ CEx4/ConsoleCEx4.cs
@@ -0,0 +1,22 @@
+﻿namespace CEx4
+{
+    internal class ConsoleCEx4
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro numero: ");
+            int num1 = int.Parse(Console.ReadLine()!);
+
+            Console.Write("\nDigite o segundo numero: ");
+            int num2 = int.Parse(Console.ReadLine()!);
+
+            Console.Write("\nDigite o terceiro numero: ");
+            int num3 = int.Parse(Console.ReadLine()!);
+
+            int maior = Math.Max(num1, num2);
+            maior = Math.Max(maior, num3);
+
+            Console.WriteLine($"\nO número {maior} é maior valor!");
+        }
+    }
+}
