6ccf96a483a0eacf2dc025f8b506bbd51d788dd3
ANDRE SILVA VIANA <andre.silva.viana.1995@gmail.com>
2025-02-27 22:21:54 -0300

    feat Exercicio 3 criado

---
+++ CEx3/ConsoleCEx3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CEx3
+{
+    internal class ConsoleCEx3
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro numero: ");
+            int num1 = int.Parse(Console.ReadLine()!);
+
+            Console.Write("\nDigite o segundo numero: ");
+            int num2 = int.Parse(Console.ReadLine()!);
+
+            int maior = Math.Max(num1, num2);
+
+            Console.WriteLine($"\nO número {maior} é maior valor!");
+        }
+    }
+}
