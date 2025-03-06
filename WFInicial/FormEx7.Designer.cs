0a99f95308b8ccb69d6a6067237fff4ff0610a0a
ANDRE SILVA VIANA <andre.silva.viana.1995@gmail.com>
2025-03-06 19:55:22 -0300

    feat: Corrigido o excercio 7

--- WFInicial/FormEx7.Designer.cs
+++ WFInicial/FormEx7.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormEx7";
+            btb_cadastrar = new Button();
+            lbl_produtos = new Label();
+            txt_produtos = new TextBox();
+            txt_valores = new TextBox();
+            lbl_valores = new Label();
+            SuspendLayout();
+            // 
+            // btb_cadastrar
+            // 
+            btb_cadastrar.Location = new Point(118, 102);
+            btb_cadastrar.Name = "btb_cadastrar";
+            btb_cadastrar.Size = new Size(75, 23);
+            btb_cadastrar.TabIndex = 0;
+            btb_cadastrar.Text = "Cadastrar";
+            btb_cadastrar.UseVisualStyleBackColor = true;
+            btb_cadastrar.Click += button1_Click;
+            // 
+            // lbl_produtos
+            // 
+            lbl_produtos.AutoSize = true;
+            lbl_produtos.Location = new Point(34, 32);
+            lbl_produtos.Name = "lbl_produtos";
+            lbl_produtos.Size = new Size(58, 15);
+            lbl_produtos.TabIndex = 1;
+            lbl_produtos.Text = "Produtos:";
+            // 
+            // txt_produtos
+            // 
+            txt_produtos.Location = new Point(93, 24);
+            txt_produtos.Name = "txt_produtos";
+            txt_produtos.Size = new Size(100, 23);
+            txt_produtos.TabIndex = 2;
+            // 
+            // txt_valores
+            // 
+            txt_valores.Location = new Point(93, 63);
+            txt_valores.Name = "txt_valores";
+            txt_valores.Size = new Size(100, 23);
+            txt_valores.TabIndex = 4;
+            txt_valores.TextChanged += textBox1_TextChanged;
+            // 
+            // lbl_valores
+            // 
+            lbl_valores.AutoSize = true;
+            lbl_valores.Location = new Point(34, 71);
+            lbl_valores.Name = "lbl_valores";
+            lbl_valores.Size = new Size(47, 15);
+            lbl_valores.TabIndex = 3;
+            lbl_valores.Text = "Valores:";
+            // 
+            // FormEx7
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(282, 183);
+            Controls.Add(txt_valores);
+            Controls.Add(lbl_valores);
+            Controls.Add(txt_produtos);
+            Controls.Add(lbl_produtos);
+            Controls.Add(btb_cadastrar);
+            Name = "FormEx7";
+            Text = "FormEx7";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btb_cadastrar;
+        private Label lbl_produtos;
+        private TextBox txt_produtos;
+        private TextBox txt_valores;
+        private Label lbl_valores;
     }
 }
--- WFInicial/FormEx7.cs
+++ WFInicial/FormEx7.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.DirectoryServices.ActiveDirectory;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,36 @@ namespace WFInicial
 {
     public partial class FormEx7 : Form
     {
+        public string[] nomes = new string[2];
+        public double[] valores = new double[2];
+        public int contador = 0;
+
         public FormEx7()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            contador = contador + 1;
+            nomes[contador - 1] = txt_produtos.Text;
+            valores[contador - 1] = Convert.ToDouble(txt_valores.Text);
+
+            txt_produtos.Text = "";
+            txt_valores.Text = "";
+
+            if (contador == 2)
+            {
+                double menor = valores.Min();
+                int pos_menor = Array.IndexOf(valores, menor);
+                string mensagem = $"O produto mais barato é é {nomes[pos_menor]} de valor R$ {menor:F2}";
+                MessageBox.Show(mensagem);
+            }
         }
     }
 }
