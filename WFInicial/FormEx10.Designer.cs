ea93489cc0a613de6ba875f649e620fdcd6a6574
ANDRE SILVA VIANA <andre.silva.viana.1995@gmail.com>
2025-03-07 19:55:14 -0300

    feat: criação ex10

--- WFInicial/FormEx10.Designer.cs
+++ WFInicial/FormEx10.Designer.cs
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormEx10";
+            txt_resultado = new TextBox();
+            lbl_numero = new Label();
+            txt_numero = new TextBox();
+            SuspendLayout();
+            // 
+            // txt_resultado
+            // 
+            txt_resultado.Location = new Point(63, 75);
+            txt_resultado.Multiline = true;
+            txt_resultado.Name = "txt_resultado";
+            txt_resultado.Size = new Size(203, 325);
+            txt_resultado.TabIndex = 0;
+            // 
+            // lbl_numero
+            // 
+            lbl_numero.AutoSize = true;
+            lbl_numero.Location = new Point(63, 30);
+            lbl_numero.Name = "lbl_numero";
+            lbl_numero.Size = new Size(54, 15);
+            lbl_numero.TabIndex = 1;
+            lbl_numero.Text = "Numero:";
+            // 
+            // txt_numero
+            // 
+            txt_numero.Location = new Point(151, 27);
+            txt_numero.Name = "txt_numero";
+            txt_numero.Size = new Size(115, 23);
+            txt_numero.TabIndex = 2;
+            txt_numero.TextChanged += txt_numero_TextChanged;
+            txt_numero.Leave += txt_numero_Leave;
+            // 
+            // FormEx10
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(341, 450);
+            Controls.Add(txt_numero);
+            Controls.Add(lbl_numero);
+            Controls.Add(txt_resultado);
+            Name = "FormEx10";
+            Text = "FormEx10";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txt_resultado;
+        private Label lbl_numero;
+        private TextBox txt_numero;
     }
 }
--- WFInicial/FormEx10.cs
+++ WFInicial/FormEx10.cs
@@ -10,11 +10,29 @@ using System.Windows.Forms;
 
 namespace WFInicial
 {
-    public partial class FormEx10: Form
+    public partial class FormEx10 : Form
     {
         public FormEx10()
         {
             InitializeComponent();
+        }
+
+        private void txt_numero_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_numero_Leave(object sender, EventArgs e)
+        {
+            int numero = Convert.ToInt32(txt_numero.Text);
+            string? tabuada = "";
+
+            for(int i = 1; i < 10; i++)
+            {
+                tabuada = tabuada + $"{numero} x {i} = {numero * i}\r\n";
+            }
+            txt_resultado.Text = tabuada;
+
         }
     }
 }
